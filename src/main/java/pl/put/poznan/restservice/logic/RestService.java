e385bd967f9a61e08e2ae883a7377ce1ab1ebcda
dysnomya <dysnomya@DESKTOP-06S2JI1>
2024-11-28 14:26:52 +0100

    Model the scenario structure

--- src/main/java/pl/put/poznan/restservice/logic/RestService.java
+++ src/main/java/pl/put/poznan/restservice/logic/RestService.java
@@ -7,6 +7,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 public class RestService {
+    public record Scenariusz(String tytul, String[] aktorzy, String aktorSystemowy, Krok[] scenariusz) { }
 
+    public record Krok(String krok, IfStep ifStep, ElseStep elseStep, ForEachStep forEachStep) {}
 
+    public record IfStep(String warunek, Krok[] scenariusz) { }
+
+    public record ElseStep(Krok[] scenariusz) { }
+
+    public record ForEachStep(String element, Krok[] scenariusz) { }
 }
