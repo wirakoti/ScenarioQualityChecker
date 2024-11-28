47c6d4b998823b9dfed7dcb9bac75d3858583170
CzechowskiMateusz <mati.czechow@wp.pl>
2024-11-28 19:57:45 +0100

    Dodanie funkcjonalności REST API - JSON jako plik wejściowy.

--- src/main/java/pl/put/poznan/restservice/rest/RestServiceController.java
+++ src/main/java/pl/put/poznan/restservice/rest/RestServiceController.java
@@ -1,4 +1,39 @@
 package pl.put.poznan.restservice.rest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.restservice.logic.RestService;
 
+import java.util.Arrays;
+
+
+@RestController
+@RequestMapping("/RestService")
 public class RestServiceController {
+
+    private static final Logger logger = LoggerFactory.getLogger(RestServiceController.class);
+
+    private final RestService restService;
+
+    @Autowired
+    public RestServiceController(RestService restService) {
+        this.restService = restService;
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST, value = "/jsonmap", produces = "application/json")
+    public String post(@RequestBody RestService.Scenariusz scen) {
+
+        // log the parameters
+        logger.debug(scen.tytul());
+
+        return restService.PrzetwarzanieScenariusza(scen);
+
+    }
+
+
+
 }
+
+
