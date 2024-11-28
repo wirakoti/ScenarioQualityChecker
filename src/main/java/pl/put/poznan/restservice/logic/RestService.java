092848af8e0ed7d75d55860f246b0bc5d9bd98cc
CzechowskiMateusz <70693943+CzechowskiMateusz@users.noreply.github.com>
2024-11-28 14:08:46 +0100

    Add files via upload

---
+++ src/main/java/pl/put/poznan/restservice/logic/RestService.java
@@ -0,0 +1,12 @@
+package pl.put.poznan.restservice.logic;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+public class RestService {
+
+
+}
---
+++ src/main/java/pl/put/poznan/restservice/rest/RestServiceController.java
@@ -0,0 +1,4 @@
+package pl.put.poznan.restservice.rest;
+
+public class RestServiceController {
+}
