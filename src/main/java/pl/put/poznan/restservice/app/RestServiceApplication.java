f0ad205c64ebd0bcd6d3a148c0fd084dd24253bf
CzechowskiMateusz <mati.czechow@wp.pl>
2024-11-28 19:57:45 +0100

    Dodanie funkcjonalności REST API - JSON jako plik wejściowy.

--- src/main/java/pl/put/poznan/restservice/app/RestServiceApplication.java
+++ src/main/java/pl/put/poznan/restservice/app/RestServiceApplication.java
@@ -3,7 +3,7 @@ package pl.put.poznan.restservice.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"pl.put.poznan.restservice.rest"})
+@SpringBootApplication(scanBasePackages = {"pl.put.poznan.restservice"})
 public class RestServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
--- src/main/java/pl/put/poznan/restservice/logic/RestService.java
+++ src/main/java/pl/put/poznan/restservice/logic/RestService.java
@@ -2,10 +2,13 @@ package pl.put.poznan.restservice.logic;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Service
 public class RestService {
     public record Scenariusz(String tytul, String[] aktorzy, String aktorSystemowy, Krok[] scenariusz) { }
 
@@ -16,4 +19,18 @@ public class RestService {
     public record ElseStep(Krok[] scenariusz) { }
 
     public record ForEachStep(String element, Krok[] scenariusz) { }
+
+    public Scenariusz PrzetwarzanieScenariusza(String json) throws  Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(json, Scenariusz.class);
+    }
+
+    public String PrzetwarzanieScenariusza(Scenariusz scenariusz) {
+        StringBuilder sb = new StringBuilder();
+        int i = 1;
+
+        sb.append("Tytuł: ").append(scenariusz.tytul).append("\n");
+
+        return sb.toString();
+    }
 }
