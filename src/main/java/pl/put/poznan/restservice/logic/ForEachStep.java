b60034668aa832e5a6a433f3f0dc4baaf3f10b5d
CzechowskiMateusz <mati.czechow@wp.pl>
2024-12-10 18:22:06 +0100

    Ograniczenie funkcjonalności Wizytatora. Modyfikacja numerowania Scenariuszy - domyślne zliczanie na bazie stosu

--- src/main/java/pl/put/poznan/restservice/logic/ForEachStep.java
+++ src/main/java/pl/put/poznan/restservice/logic/ForEachStep.java
@@ -3,8 +3,5 @@ package pl.put.poznan.restservice.logic;
 public record ForEachStep(String element, Step[] scenario) implements ScenarioElement {
     public void accept(ScenarioVisitor scenarioVisitor) {
         scenarioVisitor.visit(this);
-        for(Step step : scenario) {
-            step.accept(scenarioVisitor);
-        }
     }
 }
--- src/main/java/pl/put/poznan/restservice/logic/IfStep.java
+++ src/main/java/pl/put/poznan/restservice/logic/IfStep.java
@@ -1,10 +1,7 @@
 package pl.put.poznan.restservice.logic;
 
-public record IfStep(String condition, Step[] scenario) implements ScenarioElement  {
+public record IfStep(String condition, Step[] scenario, ElseStep elseStep) implements ScenarioElement  {
     public void accept(ScenarioVisitor scenarioVisitor) {
         scenarioVisitor.visit(this);
-        for(Step step : scenario) {
-            step.accept(scenarioVisitor);
-        }
     }
 }
