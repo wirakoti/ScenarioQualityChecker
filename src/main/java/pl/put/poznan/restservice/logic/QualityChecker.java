b5b4a20b0fb4377cf1fff2c9f031bc3923bd7243
Katarzyna Chojniak <chojniakaktarzyna@protonmail.com>
2025-01-25 22:58:53 +0100

    Udoskonalenie funkcji obliczającej jakość scenariusza!

--- src/main/java/pl/put/poznan/restservice/logic/QualityChecker.java
+++ src/main/java/pl/put/poznan/restservice/logic/QualityChecker.java
@@ -1,7 +1,7 @@
 package pl.put.poznan.restservice.logic;
 
 public class QualityChecker {
-    private float scenarioQuality;
+    private double scenarioQuality;
     private Scenario scenario;
 
     public QualityChecker(Scenario scenario) {
@@ -9,15 +9,35 @@ public class QualityChecker {
         this.scenarioQuality = calculateScenarioQuality();
     }
 
-    public float getScenarioQuality() {
+    public double getScenarioQuality() {
         return scenarioQuality;
     }
 
-    private float calculateScenarioQuality() {
+    private double calculateScenarioQuality() {
+        double qualityResult = 1;
+
         float stepCount = getStepCount();
         float keywordCount = getKeywordCount();
+        float ifCount = getIfKeywordCount();
 
-        return 1 - (keywordCount / stepCount);
+        if (ifCount < Math.log(stepCount)) {
+            qualityResult /= (Math.log(stepCount) - ifCount);
+        }
+
+        if (keywordCount < 2) {
+            qualityResult /= 0.5;
+        }
+
+        float noActorStepCount = getNoActorStepCount();
+        float potentialActorCount = getValidActorCount();
+
+        float actorValue = (noActorStepCount - potentialActorCount) / stepCount;
+
+        if (noActorStepCount - potentialActorCount > 0) {
+            qualityResult *= 1 - actorValue;
+        }
+
+        return qualityResult;
     }
 
     private float getStepCount() {
@@ -30,11 +50,27 @@ public class QualityChecker {
     private float getKeywordCount() {
         KeywordCountVisitor keywordCountVisitor = new KeywordCountVisitor();
         scenario.accept(keywordCountVisitor);
-        int keywordCount = keywordCountVisitor.getKeywordCount();
-        int ifKeywordCount = keywordCountVisitor.getIfKeywordCount();
-        int forEachKeywordCount = keywordCountVisitor.getForEachKeywordCount();
 
-        return keywordCount;
+        return keywordCountVisitor.getKeywordCount();
+    }
+
+    private float getIfKeywordCount() {
+        KeywordCountVisitor keywordCountVisitor = new KeywordCountVisitor();
+        scenario.accept(keywordCountVisitor);
+
+        return keywordCountVisitor.getIfKeywordCount();
+    }
+
+    private float getNoActorStepCount() {
+        NoActorStepVisitor noActorStepVisitor = new NoActorStepVisitor();
+        scenario.accept(noActorStepVisitor);
+        return noActorStepVisitor.getNonActorStep().size();
+    }
+
+    private float getValidActorCount() {
+        ValidActorVisitor validActorVisitor = new ValidActorVisitor();
+        scenario.accept(validActorVisitor);
+        return validActorVisitor.getActors().size();
     }
 
 }
