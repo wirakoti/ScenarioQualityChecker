2941cf01aff8b623ad3e10de506b7437797d0f76
Wiktoria Białasik <137646817+wirakoti@users.noreply.github.com>
2025-01-26 13:22:13 +0100

    Fix systemActors in Scenario.java

--- src/main/java/pl/put/poznan/restservice/logic/Scenario.java
+++ src/main/java/pl/put/poznan/restservice/logic/Scenario.java
@@ -1,6 +1,6 @@
 package pl.put.poznan.restservice.logic;
 
-public record Scenario(String title, String[] actors, String[] systemActors, Step[] scenarios) implements ScenarioElement  {
+public record Scenario(String title, String[] actors, String systemActors, Step[] scenarios) implements ScenarioElement  {
     public void accept(ScenarioVisitor scenarioVisitor) {
         scenarioVisitor.visit(this);
     }
